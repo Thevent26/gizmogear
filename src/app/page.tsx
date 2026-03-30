6ae453c343562857f7bda46f9804f98f6b817a40
TheVent26 <chipuma@kusource.com>
2026-03-30 21:24:46 +0200

    Fix JSX syntax error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,6 +126,7 @@ export default function Home() {
                           </div>
                         </div>
                       )}
+                    </div>
                   </div>
                 </div>
               </article>
